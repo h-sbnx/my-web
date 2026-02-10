63393bd8915bd9fbc3749c113e941dbcf0a2a949
h-sbnx <2413200015@stu.ahmu.edu.cn>
2026-02-10 15:54:51 +0800

    Updates

---
+++ assets/NoticeView-CwqFQ0B7.js
@@ -0,0 +1,19 @@
+import{_ as b,c as f,b as e,a as t,w as c,r as n,f as w,o as g,t as p,g as d}from"./index-DCzF-j7-.js";const v={class:"notice-page"},V={class:"notice-container"},N={class:"notice-detail"},k={class:"detail-time"},x=["innerHTML"],C={__name:"NoticeView",setup(y){const r=d([{id:1,title:"关于举办第一届安徽省大学生生物医学工程创新设计大赛的第一轮通知",time:"2025-02-10",brief:"大赛报名启动、作品要求、时间安排等核心信息",content:`
+      <p>各相关高校：</p>
+      <p>为推动安徽省生物医学工程专业人才培养，提升大学生创新设计能力，决定举办第一届安徽省大学生生物医学工程创新设计大赛，现将有关事项通知如下：</p>
+      <h4>一、组织机构</h4>
+      <p>主办单位：安徽省教育厅</p>
+      <p>承办单位：安徽医科大学生物医学工程学院</p>
+      <h4>二、参赛对象</h4>
+      <p>安徽省各高校生物医学工程及相关专业全日制本科生</p>
+      <h4>三、报名时间</h4>
+      <p>2025年3月1日-4月10日</p>
+      <h4>四、作品提交</h4>
+      <p>请于2025年4月15日前提交完整作品包至指定平台</p>
+    `},{id:2,title:"大赛报名截止时间延期通知",time:"2025-03-20",brief:"因部分高校反馈报名时间紧张，报名截止时间延期至4月15日",content:`
+      <p>各参赛高校：</p>
+      <p>应各高校参赛团队要求，经大赛组委会研究决定，现将第一届安徽省大学生生物医学工程创新设计大赛报名截止时间延期至2025年4月15日，作品提交截止时间不变（4月20日）。</p>
+      <p>请各参赛团队合理安排时间，按时完成报名及作品提交。</p>
+      <p>大赛组委会</p>
+      <p>2025年3月20日</p>
+    `}]),a=w(!1),o=d({}),_=s=>{Object.assign(o,s),a.value=!0};return(s,l)=>{const i=n("el-table-column"),m=n("el-table"),h=n("el-dialog");return g(),f("div",v,[e("div",V,[l[1]||(l[1]=e("h2",{class:"page-title"},"赛事通知",-1)),t(m,{data:r,border:"",style:{width:"100%"},onRowClick:_,class:"notice-table"},{default:c(()=>[t(i,{prop:"id",label:"序号",width:"80",align:"center"}),t(i,{prop:"title",label:"通知标题","min-width":"400"}),t(i,{prop:"time",label:"发布时间",width:"180",align:"center"}),t(i,{prop:"brief",label:"内容简介","min-width":"300"})]),_:1},8,["data"]),t(h,{modelValue:a.value,"onUpdate:modelValue":l[0]||(l[0]=u=>a.value=u),title:"通知详情",width:"70%","destroy-on-close":""},{default:c(()=>[e("div",N,[e("h3",null,p(o.title),1),e("p",k,"发布时间："+p(o.time),1),e("div",{class:"detail-content",innerHTML:o.content},null,8,x)])]),_:1},8,["modelValue"])])])}}},L=b(C,[["__scopeId","data-v-a2ee3559"]]);export{L as default};
