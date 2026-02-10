a158b05452587a9f51da946d438ac6a01a2f114a
h-sbnx <2413200015@stu.ahmu.edu.cn>
2026-02-10 16:53:56 +0800

    修复首页通知打不来bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,7 +31,7 @@
         <el-card header="大赛通知" class="module-card">
           <el-list>
             <el-list-item>
-              <a href="`${import.meta.env.BASE_URL}templates/${encodeURIComponent('关于举办第一届安徽省大学生生物医学工程创新设计大赛的第一轮通知.doc')}`" target="_blank">关于举办第一届安徽省大学生生物医学工程创新设计大赛的第一轮通知</a>
+              <a :href="`${import.meta.env.BASE_URL}templates/${encodeURIComponent('关于举办第一届安徽省大学生生物医学工程创新设计大赛的第一轮通知.doc')}`" target="_blank">关于举办第一届安徽省大学生生物医学工程创新设计大赛的第一轮通知</a>
             </el-list-item>
             <br><br>
 <!--            <el-list-item>-->
